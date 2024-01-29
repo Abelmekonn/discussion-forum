14d9b1b509f630599406dd26efdb2313e196d6a6
Abelmekonn <123172904+Abelmekonn@users.noreply.github.com>
2024-01-29 22:03:44 +0300

    Update urls.py

--- discussionforum/forumapp/urls.py
+++ discussionforum/forumapp/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',  
         views.activate, name='activate'), 
     path('',views.home,name="home"),
-    path('logout',views.logout,name="logout")
+    path('logout',views.logout,name="logout"),
+    path('posts/<str:category>/', views.posts_by_category, name="posts_by_category"),
+    path('posts/detail/<str:pk>', views.detail, name="detail"),
+    path('upload',views.upload_post,name="upload_post"),
 ]
